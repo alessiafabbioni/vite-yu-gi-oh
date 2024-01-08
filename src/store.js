14b4e5e9ec4bd079ae69d50be2bb13b3d97d0c92
alessiafabbioni <alessiafabbioni@gmail.com>
2024-01-08 17:10:56 +0100

    FIX: update of right API call

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue'
 
 export const store = reactive ({
     cardList: [],
-    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
     //searchText: "",
     //loading: true,
 });
