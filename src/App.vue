69dfc31708ff44cbb2089e90ba11b908a377d913
alessiafabbioni <alessiafabbioni@gmail.com>
2024-01-08 16:01:54 +0100

    ADD: Section add

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script >
 import AppHeader from './components/AppHeader.vue'
+import AppSearch from './components/AppSearch.vue'
+import CharacterList from './components/CharacterList.vue'
 
 export default {
   components: {
-      AppHeader
+      AppHeader,
+      AppSearch,
+      CharacterList
   }
 }
 
@@ -12,6 +16,8 @@ export default {
 <template>
   <AppHeader message="Yu-Gi-Oh"/>
   <main>
+    <AppSearch/>
+    <CharacterList/>
 
   </main>
 </template>
